9ee10169b3b2f02bad3ec0812398015ce2fe7800
carlosgbrs <carlosgabrielrsantos84@gmail.com>
2025-07-13 18:31:52 -0300

    Ajustes
    Segue o started

--- server/src/main/java/com/gdonati/EstacioneAi/repository/ClientRepository.java
+++ server/src/main/java/com/gdonati/EstacioneAi/repository/ClientRepository.java
@@ -2,6 +2,8 @@ package com.gdonati.EstacioneAi.repository;
 
 import com.gdonati.EstacioneAi.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ClientRepository extends JpaRepository <Client, Long> {
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
